adbf02a6a96223c2c99d2d8bb6f8450afccc49c9
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-09-17 18:01:06 -0500

    Crear un script de Python

---
+++ main.py
@@ -0,0 +1,11 @@
+# Este es mi primer bloque de código en Python
+MI_NOMBRE = "Mabel"
+print(f"¡Hola, mundo! Mi nombre es {MI_NOMBRE}")
+
+
+# Sintáxis de una función en Python
+def suma(a,b):
+    return a+b
+
+RESULT = suma (1,2)
+print(f"El resultado es {RESULT}")
