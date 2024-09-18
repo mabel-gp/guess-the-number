e0a675a1561f909daf0269ac1b3d8d8bd7347d86
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-09-18 11:00:30 -0500

    Proporcionar pista sobre si numero es mayor o menor

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-# Generar un número aleatorio entre 1 y 100. Usar la función randint en el módulo random
+# TAREA: Generar un número aleatorio entre 1 y 100. Usar la función randint en el módulo random
 
+# Importar el módulo random
 import random
 
-# Cambié el rango de números aleatorios
-numero_aleatorio = random.randint(1,5)
-print(numero_aleatorio)
+numero_aleatorio = random.randint(1,100)
+print(numero_aleatorio) # Después borrar esta línea
 
-# Implementa un ciclo que solicite a la jugadora que adivine el número. Usar función input()
+
+# TAREA: Implementa un ciclo que solicite a la jugadora que adivine el número, usar función input()
+# TAREA: Si la jugadora adivina correctamente, termina el juego
+# TAREA: Si la jugadora adivina incorrectamente, proporciona pista sobre si el número es mayor o menor
 
 # Implementar el input para que la usuaria elija un número
 numero_ingresado = int(input("Escribe un numero"))
 
-# Mientras el número ingresado sea diferente al numero aleatorio, continua el ciclo
+# Mientras(while) el número ingresado sea diferente al numero aleatorio, continua el ciclo
 while numero_ingresado != numero_aleatorio:
-    print("El número es incorrecto ¡Intenta de nuevo!")
-    numero_ingresado = int(input("Escribe un numero"))
-    
+    if numero_ingresado < numero_aleatorio:
+        print("El número es mayor ¡Intenta de nuevo!")
+        numero_ingresado = int(input("Escribe un numero"))
+    elif numero_ingresado > numero_aleatorio:
+        print ("El numero es menor ¡Intenta de nuevo!")
+        numero_ingresado = int(input("Escribe un numero"))
+
+# Esta línea se ejecuta cuando el número ingresado es igual número aleatorio
 print("El número es correcto ¡GANASTE!")    
+
+
 
