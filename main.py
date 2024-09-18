fc8c4232ec1db3f5aaa74fd7e2568cb57a11eece
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-09-17 19:06:17 -0500

    Generar numero aleatorio

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# Este es mi primer bloque de código en Python
-MI_NOMBRE = "Mabel"
-print(f"¡Hola, mundo! Mi nombre es {MI_NOMBRE}")
+# Generar un número aleatorio entre 1 y 100. Usar la función randint en el módulo random.
 
+import random
 
-# Sintáxis de una función en Python
-def suma(a,b):
-    return a+b
+numero_aleatorio = random.randint(1,100)
+print(numero_aleatorio)
 
-RESULT = suma (1,2)
-print(f"El resultado es {RESULT}")
