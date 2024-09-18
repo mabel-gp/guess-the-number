71fd0573b8ff51998fd2fdf0a64c9abf23912185
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-09-18 13:09:44 -0500

    Implementar logica para el turno de la computadora

--- main.py
+++ main.py
@@ -4,27 +4,51 @@
 import random
 
 numero_aleatorio = random.randint(1,100)
-print(numero_aleatorio) # Después borrar esta línea
+#print(numero_aleatorio)
 
 
 # TAREA: Implementa un ciclo que solicite a la jugadora que adivine el número, usar función input()
 # TAREA: Si la jugadora adivina correctamente, termina el juego
 # TAREA: Si la jugadora adivina incorrectamente, proporciona pista sobre si el número es mayor o menor
+# TAREA: Implementar la lógica para el turno del ordenador
 
-# Implementar el input para que la usuaria elija un número
-numero_ingresado = int(input("Escribe un numero"))
+# Lógica para la jugadora
+def jugadora():
+
+    numero_ingresado = int(input("Escribe un numero"))
 
 # Mientras(while) el número ingresado sea diferente al numero aleatorio, continua el ciclo
-while numero_ingresado != numero_aleatorio:
-    if numero_ingresado < numero_aleatorio:
-        print("El número es mayor ¡Intenta de nuevo!")
-        numero_ingresado = int(input("Escribe un numero"))
-    elif numero_ingresado > numero_aleatorio:
-        print ("El numero es menor ¡Intenta de nuevo!")
-        numero_ingresado = int(input("Escribe un numero"))
+    while numero_ingresado != numero_aleatorio:
+
+        if numero_ingresado < numero_aleatorio:
+            print("El número es mayor ¡Jugadora intenta de nuevo!")
+            numero_ingresado = int(input("Escribe un numero"))
+        elif numero_ingresado > numero_aleatorio:
+            print ("El numero es menor ¡Jugadora intenta de nuevo!")
+            numero_ingresado = int(input("Escribe un numero"))
 
 # Esta línea se ejecuta cuando el número ingresado es igual número aleatorio
-print("El número es correcto ¡GANASTE!")    
+    print("El número es correcto ¡JUGADORA GANA!")    
+
+jugadora()
 
 
+# Lógica para la computadora
+def computadora():
 
+    numero_computadora = random.randint(1,100)
+
+    while numero_computadora != numero_aleatorio:
+
+        print(f"Computadora elige el número {numero_computadora}")
+
+        if numero_computadora < numero_aleatorio:
+            print("El número es mayor ¡Computadora intenta de nuevo!")
+        elif numero_aleatorio > numero_aleatorio:
+            print ("El numero es menor ¡Computadora intenta de nuevo!")
+
+        numero_computadora = random.randint(1,100)
+
+    print ("El número es correcto ¡COMPUTADORA GANA!")
+
+computadora()
