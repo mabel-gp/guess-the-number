223982f958f516c90f5ca74b988fab922e322122
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-09-20 23:55:16 -0500

    Modificar registro de suposiciones e incorporar funciones separadas

--- main.py
+++ main.py
@@ -1,57 +1,64 @@
-# 1. Generar un número aleatorio entre 1 y 100. Usar la función randint en el módulo random
-# 2. Implementa un ciclo que solicite a la jugadora que adivine el número, usar función input()
-# 3. Si la jugadora adivina correctamente, termina el juego
-# 4. Si la jugadora adivina incorrectamente, proporciona pista sobre si el número es mayor o menor
-# 5. Implementar la lógica para el turno del ordenador
-# 6. Continúa el juego hasta que la jugadora o la computadora adivinen correctamente
-# 7. Mostrar registro de suposiciones
 
 # Importar el módulo random
 import random
 
-numero_aleatorio = random.randint(1, 100)
-print(numero_aleatorio)
+# Variables de alcance global
+intentos_jugadora = []
+intentos_computadora = []
 
 def juego():
     
-    intentos = 0
+    numero_aleatorio = random.randint(1, 100)
+    #print(numero_aleatorio)
+    print('\n¡Hola! Elige un número entre 1 y 100\n')
+    
     # El bucle continúa hasta que encuentra al comando break
     while True:
-    
-        lista_intentos = []
-        lista_intentos.append(intentos)
+
+        if jugadora(numero_aleatorio):
+            break
+       
+        if computadora(numero_aleatorio):  
+            break
+
+
+def jugadora(numero_aleatorio):
+
+    numero_jugadora = int(input("Escribe tu número aquí: "))
+    intentos_jugadora.append(numero_jugadora)
+
+    if numero_jugadora < numero_aleatorio:
+        print("El número es mayor\n¡Turno Computadora!\n")
+        return False
+    elif numero_jugadora > numero_aleatorio:
+        print("El número es menor\n¡Turno Computadora!\n")
+        return False
+    else:
+        print("¡JUGADORA GANA!")
+        # Con .join se accede a los elementos de la lista, usando antes map para convertirlos en string
+        print (f"Estos son tus intentos: {' - '.join(map(str,intentos_jugadora))}\n¡FELICIDADES!\n")
+        return True
+
+
+def computadora(numero_aleatorio): 
+
+    numero_computadora = random.randint(1, 100)
+    intentos_computadora.append(numero_computadora)
+    print(f"Computadora elige el número {numero_computadora}")
         
-        # TURNO JUGADORA
 
-        numero_jugadora = int(input("Escribe un número entre 1 y 100: "))
-        intentos = intentos + 1
-
-        if numero_jugadora < numero_aleatorio:
-            print("El número es mayor\n¡Turno Computadora!\n")
-        elif numero_jugadora > numero_aleatorio:
-            print("El numero es menor\n¡Turno Computadora!\n")
-        else:
-            print("¡JUGADORA GANA!")
-            print (f"Lo intentaste {lista_intentos[0]} veces antes de lograrlo. ¡Felicidades!")
-            print("Fin del juego")
-            # El bucle se detiene dado que el numero de la jugadora es igual al aleatorio 
-            break 
+    if numero_computadora < numero_aleatorio:
+        print("El número es mayor\n¡Turno Jugadora!\n")
+        return False
+    elif numero_computadora > numero_aleatorio:
+        print("El número es menor\n¡Turno Jugadora!\n")
+        return False
+    else:
+        print("¡COMPUTADORA GANA!")
+        print (f"Estos son los intentos de Computadora: {' - '.join(map(str,intentos_computadora))}\n¡FELICIDADES")
+        return True
 
 
-        # TURNO COMPUTADORA
-
-        numero_computadora = random.randint(1, 100)
-        print(f"Computadora elige el número {numero_computadora}")
-
-        if numero_computadora == numero_aleatorio:
-            print("¡COMPUTADORA GANA!")
-            print (f"Computadora lo intentó {lista_intentos[0]} veces antes de lograrlo. ¡Felicidades!")
-            print("Fin del juego")
-            # El bucle se detiene dado que el número de la computadora es igual al aleatorio
-            break 
-        else:
-            print("El número que eligió Computadora es incorrecto\n¡Turno Jugadora!\n")
-            
+# Se llama a la función juego() al final después de que las demás funciones fueron declaradas
 juego()
-
 
